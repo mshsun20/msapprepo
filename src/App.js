e9739c949f69a88f75e50b6290952c64af6239ae
Sunny Halder <msh.sun20@gmail.com>
2021-07-25 03:03:28 +0530

    updated 3.03am 25july2021

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import './styles/mystyl.scss'
+import './styles/apbodystyl.scss'
+import './styles/navstyl.scss'
+import './styles/fotrstyl.scss'
 import Router from './Router'
 
 const App = () => {
